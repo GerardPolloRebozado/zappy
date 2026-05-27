5217c6c77287397ef8390c4d778601b4b833ac68
Ana <116268873+smartysharky@users.noreply.github.com>
2026-05-27 19:22:04 +0200

    docs: added function documentation

--- ai/client.py
+++ ai/client.py
@@ -3,20 +3,40 @@ import socket
 
 
 def connect(port, name, ip):
+    """
+    Connects to the Zappy serve using the port, name and ip address
+    :param port: port number to connect to
+    :param name: name of the team
+    :param ip: ip address
+    :return: 0 or 84 on error
+    """
     s = socket.socket()
 
-    s.connect((ip, port))
+    try:
+        s.connect((ip, port))
+    except ConnectionRefusedError:
+        print(f"Could not connect to server at {ip}:{port}")
+        return 84
 
     print(s.recv(1024).decode())
 
     s.close()
+    return 0
 
 
 def print_help():
+    """
+    Prints help message
+    :return: nothing
+    """
     print("USAGE: ./zappy_ai -p port -n name -ip ip address")
 
 
 def main():
+    """
+    Main function, reads the command line arguments and calls the connect function
+    :return: 0 or 84 on error
+    """
     params = sys.argv[1:]
 
     if params == []:
