5e999b1e823ddf64fa58d6b256702575e9a43088
Pol Cortadellas Palomino <pol.cortadellas-palomino@epitech.eu>
2026-05-28 07:13:45 +0200

    feat: refactor basic components

---
+++ gui/include/Components/ComponentShared.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy_gui
+** File description:
+** ComponentShared.hpp
+*/
+#ifndef ZAPPY_GUI_COMPONENTSHARED_HPP
+#define ZAPPY_GUI_COMPONENTSHARED_HPP
+
+struct Position {
+    int x;
+    int y;
+};
+
+struct Inventory {
+    int food;
+    int linemate;
+    int deraumere;
+    int sibur;
+    int mendiane;
+    int phiras;
+    int thystame;
+};
+
+#endif //ZAPPY_GUI_COMPONENTSHARED_HPP
