669f2a5e107f6218b9e72f503f4a2e892003740f
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-05-28 10:45:09 +0200

    chore: add empty files for compilation

---
+++ gui/include/IElement.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy_gui
+** File description:
+** IElement.hpp
+*/
+
+#ifndef IELEMENT_HPP_
+#define IELEMENT_HPP_
+
+namespace zappy {
+class IElement {
+  public:
+    virtual ~IElement() = default;
+};
+} // namespace zappy
+
+#endif /* !IELEMENT_HPP_ */
