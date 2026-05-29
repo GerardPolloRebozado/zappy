05bbfd1d7826143bb2359171ecc0b32c4c6f942c
nicoepitech <nicolas.balzamo@epitech.eu>
2026-05-29 15:32:10 +0200

    refactor: .gitignore to avoid cmake files

--- server/src/common/protocol/response.rs
+++ server/src/common/protocol/response.rs
@@ -1,11 +1,9 @@
-use crate::common::protocol::event::EventCode;
 use crate::common::protocol::status::StatusCode;
 use std::str::FromStr;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ResponseCode {
     Status(StatusCode),
-    Event(EventCode),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -42,32 +40,6 @@ impl FromStr for Response {
             });
         }
 
-        let parts: Vec<&str> = s.split_whitespace().collect();
-        let prefix = parts[0];
-
-        let event = match prefix {
-            "message" => Some(EventCode::Message),
-            "eject:" => Some(EventCode::Eject),
-            "dead" => Some(EventCode::Dead),
-            "pnw" => Some(EventCode::Pnw),
-            "pdi" => Some(EventCode::Pdi),
-            "ppo" => Some(EventCode::Ppo),
-            "pie" => Some(EventCode::Pie),
-            "seg" => Some(EventCode::Seg),
-            _ => None,
-        };
-
-        if let Some(e) = event {
-            return Ok(Response {
-                code: ResponseCode::Event(e),
-                data: if parts.len() > 1 {
-                    Some(parts[1..].join(" "))
-                } else {
-                    None
-                },
-            });
-        }
-
         Ok(Response {
             code: ResponseCode::Status(StatusCode::Ok),
             data: Some(s.to_string()),
@@ -80,29 +52,12 @@ impl std::fmt::Display for Response {
         match self.code {
             ResponseCode::Status(StatusCode::Ok) => {
                 if let Some(ref data) = self.data {
-                    writeln!(f, "{}", data)
+                    write!(f, "{}", data)
                 } else {
                     writeln!(f, "ok")
                 }
             }
             ResponseCode::Status(StatusCode::Ko) => writeln!(f, "ko"),
-            ResponseCode::Event(e) => {
-                let prefix = match e {
-                    EventCode::Message => "message",
-                    EventCode::Eject => "eject:",
-                    EventCode::Dead => "dead",
-                    EventCode::Pnw => "pnw",
-                    EventCode::Pdi => "pdi",
-                    EventCode::Ppo => "ppo",
-                    EventCode::Pie => "pie",
-                    EventCode::Seg => "seg",
-                };
-                if let Some(ref data) = self.data {
-                    writeln!(f, "{} {}", prefix, data)
-                } else {
-                    writeln!(f, "{}", prefix)
-                }
-            }
         }
     }
 }
