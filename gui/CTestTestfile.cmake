15a44576c73626fcffd51e88e523b19b4238643c
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-01 19:22:15 +0200

    feat: camera movement and rotation

---
+++ gui/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/epitech/tek2/g-yep-400/zappy/gui
+# Build directory: /home/alex/epitech/tek2/g-yep-400/zappy/gui
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../_deps/raylib-build")
+subdirs("../_deps/raylib-cpp-build")
