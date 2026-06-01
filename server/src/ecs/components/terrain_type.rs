210a8b1a557c4641a870442938138358655892bd
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-01 16:35:19 +0200

    feat: map generation from perlin noise

--- server/src/ecs/components/terrain_type.rs
+++ server/src/ecs/components/terrain_type.rs
@@ -1,6 +1,16 @@
 /// Terrain type will be used to describe the terrain of each tile of the map
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum TerrainType {
+    // Standard Biomes
     Grass,
     Mountain,
     Water,
+    Sand,
+    Forest,
+
+    // Special Biomes (Trantor)
+    ObsidianBarrens,
+    LuminousOrchards,
+    CrystalCanyons,
+    MagneticTundra,
 }
