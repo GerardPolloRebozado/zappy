a56f2bc2f67771bad2187ed58849263d1edf5281
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-01 19:22:50 +0200

    chore: remove makefiles

--- gui/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/epitech/tek2/g-yep-400/zappy/gui
-# Build directory: /home/alex/epitech/tek2/g-yep-400/zappy/gui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/raylib-build")
-subdirs("../_deps/raylib-cpp-build")
