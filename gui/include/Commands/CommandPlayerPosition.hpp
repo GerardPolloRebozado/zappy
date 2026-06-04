b3d71d8ed82fb7189f9e5a95b525fc5a66c6cec0
Pol Cortadellas Palomino <pol.cortadellas-palomino@epitech.eu>
2026-06-04 10:35:50 +0200

    feat: command player position

--- gui/include/Commands/CommandPlayerPosition.hpp
+++ gui/include/Commands/CommandPlayerPosition.hpp
@@ -1,26 +1,78 @@
 /*
 ** EPITECH PROJECT, 2026
-** zappy
+** zappy_gui
 ** File description:
 ** CommandPlayerPosition.hpp
 */
+
 #ifndef ZAPPY_COMMANDPLAYERPOSITION_HPP
 #define ZAPPY_COMMANDPLAYERPOSITION_HPP
+
 #include "ACommand.hpp"
+#include "ECS/World.hpp"
+#include "ECS/ComponentMap.hpp"
+#include "Components/ComponentShared.hpp"
+#include "Components/ComponentTags.hpp"
+
+#include <algorithm>
+#include <sstream>
+
+#include "Components/ComponentInhabitant.hpp"
 
 namespace zappy {
     class CommandPlayerPosition : public ACommand {
-        CommandPlayerPosition()  = default;
-        /**
-         * @brief Requests the current position of a specific player.
-         * @param args The arguments for the command, expected to contain the player ID
-         * @param registry The registry containing the application state, where the player's position will be updated
-         */
-        void execute(const std::string&args, Register&registry) override
-        {
+    public:
+        CommandPlayerPosition() = default;
+        ~CommandPlayerPosition() override = default;
 
-        };
+        /**
+         * @brief Handles the "ppo" command, updating a player's position and orientation.
+         * @param args The arguments for the command, expected to be "n X Y O" or "#n X Y O"
+         * @param world The ECS World containing the application state
+         */
+        void execute(const std::string& args, World& world) override {
+            std::string cleanArgs = args;
+            cleanArgs.erase(std::remove(cleanArgs.begin(), cleanArgs.end(), '#'), cleanArgs.end());
+
+            std::istringstream iss(cleanArgs);
+            int playerId, x, y, orientation;
+
+            if (!(iss >> playerId >> x >> y >> orientation)) {
+                return;
+            }
+            auto InhabitantStorage = world.get_storage<InhabitantData>();
+            if (!InhabitantStorage) {
+                return;
+            }
+            bool found = false;
+            Entity targetEntity{0, 0};
+
+            for (auto const& [entity, Inhabitant] : *InhabitantStorage) {
+                if (Inhabitant->id == playerId) {
+                    targetEntity = entity;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                auto pos = world.get_component<Position>(targetEntity);
+                if (pos) {
+                    pos->x = x;
+                    pos->y = y;
+                } else {
+                    world.add_component<Position>(targetEntity, Position{x, y});
+                }
+
+                auto Inhabitant = world.get_component<InhabitantData>
+                (targetEntity);
+                if (Inhabitant) {
+                    Inhabitant->orientation = orientation;
+                }
+            } else {
+            }
+        }
     };
-} // zappy
+} // namespace zappy
 
 #endif //ZAPPY_COMMANDPLAYERPOSITION_HPP
--- gui/src/Systems/RenderSystem.cpp
+++ gui/src/Systems/RenderSystem.cpp
@@ -11,7 +11,7 @@
  */
 
 #include "Systems/RenderSystem.hpp"
-#include "Components/ComponentBot.hpp"
+#include "Components/ComponentInhabitant.hpp"
 #include "Components/ComponentShared.hpp"
 #include "Components/ComponentTile.hpp"
 #include "ECS/World.hpp"
@@ -211,7 +211,7 @@ void RenderSystem::_renderTerrain(World& w) {
 }
 
 void RenderSystem::_renderInhabitants(World& w) {
-    auto botStorage = w.get_storage<BotData>();
+    auto botStorage = w.get_storage<InhabitantData>();
     if (!botStorage) {
         return;
     }
