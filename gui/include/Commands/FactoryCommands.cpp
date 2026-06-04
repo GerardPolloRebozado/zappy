efdb222ff6be6179828a2e0191b6b23d5a0a38fc
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-04 20:13:39 +0200

    feat(gui): level command

--- gui/include/Commands/FactoryCommands.cpp
+++ gui/include/Commands/FactoryCommands.cpp
@@ -5,27 +5,30 @@
 ** FactoryCommands.cpp
 */
 #include "FactoryCommands.hpp"
-#include "Commands/FactoryCommands.hpp"
-#include "Commands/CommandMapSize.hpp"
 #include "Commands/CommandMapContent.hpp"
-#include "Commands/CommandTileContent.hpp"
+#include "Commands/CommandMapSize.hpp"
+#include "Commands/CommandPlayerLevel.hpp"
+#include "Commands/CommandPlayerPosition.hpp"
 #include "Commands/CommandTeamNames.hpp"
+#include "Commands/CommandTileContent.hpp"
+#include "Commands/FactoryCommands.hpp"
 
 namespace zappy {
 
-    const std::unordered_map<std::string, FactoryCommands::CommandCreator> FactoryCommands::_creators = {
-        {"msz", []() { return std::make_unique<CommandMapSize>(); }},
-        {"mct", []() { return std::make_unique<CommandMapContent>(); }},
-        {"bct", []() { return std::make_unique<CommandTileContent>(); }},
-        {"tna", []() { return std::make_unique<CommandTeamNames>(); }}
-    };
+const std::unordered_map<std::string, FactoryCommands::CommandCreator> FactoryCommands::_creators =
+    {{"msz", []() { return std::make_unique<CommandMapSize>(); }},
+     {"mct", []() { return std::make_unique<CommandMapContent>(); }},
+     {"bct", []() { return std::make_unique<CommandTileContent>(); }},
+     {"tna", []() { return std::make_unique<CommandTeamNames>(); }},
+     {"ppo", []() { return std::make_unique<CommandPlayerPosition>(); }},
+     {"plv", []() { return std::make_unique<CommandPlayerLevel>(); }}};
 
-    std::unique_ptr<ACommand> FactoryCommands::createCommand(const std::string& commandName) {
-        auto it = _creators.find(commandName);
+std::unique_ptr<ACommand> FactoryCommands::createCommand(const std::string& commandName) {
+    auto it = _creators.find(commandName);
 
-        if (it != _creators.end()) {
-            return it->second();
-        }
-        return nullptr;
+    if (it != _creators.end()) {
+        return it->second();
     }
+    return nullptr;
 }
+} // namespace zappy
