6189dbb3503e55131cb5dcb685d2a465c1373d7b
Gerard Du Pre <gerardit4@gmail.com>
2026-06-05 12:23:43 +0200

    feat: :sparkles: server side team validations and mock socket

--- server/src/game.rs
+++ server/src/game.rs
@@ -1,11 +1,9 @@
 pub mod date;
 pub mod inhabitant;
 pub mod resource;
-pub mod team;
 pub mod user;
 
 pub use date::Date;
 pub use inhabitant::Inhabitant;
 pub use resource::Resource;
-pub use team::Team;
 pub use user::User;
