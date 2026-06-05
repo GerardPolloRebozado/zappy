3796ae5f80686f7efa45a29aa5f9c1fe2080969d
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-05 09:20:37 +0200

    feat: player connection

---
+++ gui/include/Commands/CommandPlayerConnection.hpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** CommandPlayerConnection.hpp
+*/
+#ifndef ZAPPY_COMMANDPLAYERCONNECTION_HPP
+#define ZAPPY_COMMANDPLAYERCONNECTION_HPP
+
+#include "ACommand.hpp"
+#include "Components/ComponentInhabitant.hpp"
+#include "Components/ComponentShared.hpp"
+#include "Components/ComponentTags.hpp"
+#include <algorithm>
+#include <sstream>
+
+namespace zappy {
+class CommandPlayerConnection : public ACommand {
+  public:
+    CommandPlayerConnection() = default;
+    ~CommandPlayerConnection() override = default;
+
+    /**
+     * @brief Handles the "pnw" command, spawning a new player.
+     * @param args The arguments for the command: "#n X Y O L N"
+     * @param world The ECS World containing the application state
+     */
+    void execute(const std::string& args, World& world) override {
+        std::string cleanArgs = args;
+        cleanArgs.erase(std::remove(cleanArgs.begin(), cleanArgs.end(), '#'), cleanArgs.end());
+
+        std::istringstream iss(cleanArgs);
+        int playerId, x, y, orientation, level;
+        std::string teamName;
+
+        if (!(iss >> playerId >> x >> y >> orientation >> level >> teamName)) {
+            return;
+        }
+
+        // TODO: create player util fn?
+        Entity player = world.spawn_at_id(playerId);
+        world.add_component<Position>(player, {x, y});
+        world.add_component<Orientation>(player,
+                                         {static_cast<Orientation::Direction>(orientation)});
+        world.add_component<Level>(player, {level});
+        world.add_component<TeamName>(player, {teamName});
+        world.add_component<Inventory>(player, {0, 0, 0, 0, 0, 0, 0});
+        world.add_component<InhabitantTag>(player, {});
+
+        std::cout << "Protocol: New player #" << playerId << " connected (Team: " << teamName << ")"
+                  << std::endl;
+    }
+};
+} // namespace zappy
+
+#endif // ZAPPY_COMMANDPLAYERCONNECTION_HPP
--- gui/include/Commands/FactoryCommands.cpp
+++ gui/include/Commands/FactoryCommands.cpp
@@ -5,8 +5,11 @@
 ** FactoryCommands.cpp
 */
 #include "FactoryCommands.hpp"
+#include "Commands/CommandIncantationEnd.hpp"
+#include "Commands/CommandIncantationStart.hpp"
 #include "Commands/CommandMapContent.hpp"
 #include "Commands/CommandMapSize.hpp"
+#include "Commands/CommandPlayerConnection.hpp"
 #include "Commands/CommandPlayerInventory.hpp"
 #include "Commands/CommandPlayerLevel.hpp"
 #include "Commands/CommandPlayerPosition.hpp"
@@ -22,7 +25,10 @@ const std::unordered_map<std::string, FactoryCommands::CommandCreator> FactoryCo
      {"tna", []() { return std::make_unique<CommandTeamNames>(); }},
      {"ppo", []() { return std::make_unique<CommandPlayerPosition>(); }},
      {"plv", []() { return std::make_unique<CommandPlayerLevel>(); }},
-     {"pin", []() { return std::make_unique<CommandPlayerInventory>(); }}};
+     {"pin", []() { return std::make_unique<CommandPlayerInventory>(); }},
+     {"pnw", []() { return std::make_unique<CommandPlayerConnection>(); }},
+     {"pic", []() { return std::make_unique<CommandIncantationStart>(); }},
+     {"pie", []() { return std::make_unique<CommandIncantationEnd>(); }}};
 
 std::unique_ptr<ACommand> FactoryCommands::createCommand(const std::string& commandName) {
     auto it = _creators.find(commandName);
