03aeb43392d44aacb1ef0c7432eac30f7fc6c254
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-05 17:23:50 +0200

    feat: add asset manager to render system

--- gui/include/Graphics/AssetManager.hpp
+++ gui/include/Graphics/AssetManager.hpp
@@ -4,29 +4,47 @@
 ** File description:
 ** AssetManager.hpp
 */
+
 #ifndef ZAPPY_GUI_ASSETMANAGER_HPP
 #define ZAPPY_GUI_ASSETMANAGER_HPP
+
 #include "raylib-cpp.hpp"
+#include <map>
+#include <memory>
+#include <string>
+
 namespace zappy {
 
-    struct Assets {
-    //stones
-    raylib::Model Linemate;
-    raylib::Model Deraumere;
-    raylib::Model Sibur;
-    raylib::Model Mendiane;
-    raylib::Model Phiras;
-    raylib::Model Thystame;
-    // foods
-    raylib::Model Food;
-    //bots
-    raylib::Model TrantorianNico;
-    //tiles
-    raylib::Model TileMountain;
-    raylib::Model TileLake;
-    raylib::Model TilePlain;
+class AssetManager {
+  public:
+    static AssetManager& getInstance();
+
+    void loadAll();
+
+    // Models
+    raylib::Model& getModel(const std::string& name);
+
+    // Textures
+    raylib::Texture2D& getTexture(const std::string& name);
+
+    // Shaders
+    raylib::Shader& getShader(const std::string& name);
+
+  private:
+    AssetManager() = default;
+    ~AssetManager() = default;
+    AssetManager(const AssetManager&) = delete;
+    AssetManager& operator=(const AssetManager&) = delete;
+
+    std::map<std::string, std::unique_ptr<raylib::Model>> _models;
+    std::map<std::string, std::unique_ptr<raylib::Texture2D>> _textures;
+    std::map<std::string, std::unique_ptr<raylib::Shader>> _shaders;
+
+    void _loadModels();
+    void _loadTextures();
+    void _loadShaders();
 };
 
-} // zappy
+} // namespace zappy
 
-#endif //ZAPPY_GUI_ASSETMANAGER_HPP
+#endif // ZAPPY_GUI_ASSETMANAGER_HPP
