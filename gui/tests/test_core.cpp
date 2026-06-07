4c5f9e255a1e922e5a9d0163bb5c2e2b98ba5dfb
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-07 10:14:18 +0200

    ci: new attempt

--- gui/tests/test_core.cpp
+++ gui/tests/test_core.cpp
@@ -1,4 +1,5 @@
 #include "Core.hpp"
+#include "Graphics/AssetManager.hpp"
 #include <criterion/criterion.h>
 #include <raylib.h>
 
@@ -11,9 +12,11 @@ Test(core, initialization) {
     } catch (...) {
         cr_assert(false, "Core initialization threw an exception");
     }
+    zappy::AssetManager::getInstance().unloadAll();
 }
 
 Test(core, run_loop_exit) {
     zappy::Core core(0, "127.0.0.1");
     cr_assert(true);
+    zappy::AssetManager::getInstance().unloadAll();
 }
