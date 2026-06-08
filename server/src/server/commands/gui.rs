b87a4af61364762a2c1faf4e73f17c241592d672
Gerard Du Pre <gerardit4@gmail.com>
2026-06-08 11:35:55 +0200

    feat: :sparkles: TNA command

--- server/src/server/commands/gui.rs
+++ server/src/server/commands/gui.rs
@@ -61,6 +61,20 @@ pub fn handle_gui_command(server: &mut Server, entity: Entity, request: Request)
             }
         }
 
+        Command::Tna => {
+            let network_data = server.world.get_component_mut::<NetworkData>(entity);
+            if network_data.is_none() {
+                return;
+            }
+            let network_data = network_data.unwrap();
+            for team in server.team_names.iter() {
+                network_data.pending_responses.push(Response::new(
+                    ResponseCode::Status(StatusCode::Ok),
+                    Some(format!("tna {}", team)),
+                ));
+            }
+        }
+
         Command::Sgt => {
             let freq = server.world.freq;
             let network_data = server.world.get_component_mut::<NetworkData>(entity);
@@ -96,5 +110,56 @@ pub fn handle_gui_command(server: &mut Server, entity: Entity, request: Request)
                 .pending_responses
                 .push(Response::new(ResponseCode::Status(StatusCode::Ko), None));
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ecs::components::network::MockSocket;
+    use crate::ecs::storage::World;
+
+    #[test]
+    fn test_tna_command() {
+        let listener = std::net::TcpListener::bind("127.0.0.1:0").unwrap();
+        let mut server = Server {
+            listener,
+            _users: std::collections::HashMap::new(),
+            _freq: 100,
+            game_start: 0,
+            world: World::new(
+                crate::ecs::map_size::MapSize {
+                    width: 10,
+                    height: 10,
+                },
+                100,
+            ),
+            clients_nb: 1,
+            team_names: vec!["TeamA".to_string(), "TeamB".to_string()],
+        };
+        let entity = server.world.spawn();
+
+        let (mock_socket, _) = MockSocket::new(Vec::from(""));
+        let network_data = NetworkData::new(mock_socket);
+        server.world.add_component(entity, network_data);
+
+        handle_gui_command(
+            &mut server,
+            entity,
+            Request {
+                command: Command::Tna,
+            },
+        );
+
+        let network_data = server.world.get_component::<NetworkData>(entity).unwrap();
+        assert_eq!(network_data.pending_responses.len(), 2);
+        assert_eq!(
+            network_data.pending_responses[0].data.as_ref().unwrap(),
+            "tna TeamA"
+        );
+        assert_eq!(
+            network_data.pending_responses[1].data.as_ref().unwrap(),
+            "tna TeamB"
+        );
     }
 }
