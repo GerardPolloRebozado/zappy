058d9b94fa94e7bd53f60dd69685b83db37e499a
nicoepitech <nicolas.balzamo@epitech.eu>
2026-06-08 18:08:39 +0200

    feat: add logging macros

---
+++ gui/include/Logging/LogConfig.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy_gui
+** File description:
+** LogConfig.hpp
+*/
+
+#ifndef ZAPPY_LOG_CONFIG_HPP
+#define ZAPPY_LOG_CONFIG_HPP
+
+#define ERROR 1
+#define DEBUG 2
+#define INFO  3
+
+#ifndef ZAPPY_LOG_LEVEL
+#define ZAPPY_LOG_LEVEL INFO
+#endif
+
+#endif // ZAPPY_LOG_CONFIG_HPP
