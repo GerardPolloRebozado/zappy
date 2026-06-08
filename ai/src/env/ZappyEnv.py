7dfedd9d2039f6aee8a629fc3af32fa2418e331b
Pol Cortadellas Palomino <pol.cortadellas-palomino@epitech.eu>
2026-06-09 00:44:41 +0200

    chore: zappyenv bette action parsing and observation improvement

--- ai/src/env/ZappyEnv.py
+++ ai/src/env/ZappyEnv.py
@@ -15,6 +15,7 @@ class ZappyEnv(gym.Env):
         self.client = None
 
         self.action_space = spaces.Discrete(19)
+
         self.action_mapping = {
             1: "Forward",
             3: "Left",
@@ -30,7 +31,6 @@ class ZappyEnv(gym.Env):
             18: "Incantation",
         }
 
-        # shape = 81 vision tiles * 8 posibles recursos + 7 inventario + nivel + vida
         self.observation_space = spaces.Box(
             low=0, high=10000, shape=(657,), dtype=np.int32
         )
@@ -39,37 +39,57 @@ class ZappyEnv(gym.Env):
         super().reset(seed=seed)
 
         self.server_manager.start()
+
         self.client = ZappyAiClient(self.port, self.team_name, self.ip)
         self.client.connect()
-        self.client.receive_line()
-        self.client.receive_line()
-        self.client.receive_line()
         observation = self._get_real_observation()
         info = {}
-
         return observation, info
 
     def step(self, action):
-        command = self.action_mapping.get(action, None)
-
         reward = 0.0
         terminated = False
         truncated = False
+        response = None
 
-        if command is not None:
-            self.client.send(f"{command}\n")
-
-            response = self.client.receive().strip()
-
-            if response == "dead":
-                terminated = True
-                reward = -100.0
-            elif response == "ok":
-                reward = 1.0
-            elif response == "ko":
-                reward = -0.1
+        if action == 1:
+            response = self.client.forward()
+        elif action == 3:
+            response = self.client.left()
+        elif action == 7:
+            response = self.client.right()
+        elif action == 10:
+            response = self.client.look()
+        elif action == 11:
+            response = self.client.inventory()
+        elif action == 12:
+            response = self.client.broadcast("Hola")
+        elif action == 13:
+            response = self.client.connect_nbr()
+        elif action == 14:
+            response = self.client.fork()
+        elif action == 15:
+            response = self.client.eject()
+        elif action == 16:
+            response = self.client.take(
+                "food"
+            )  # TODO: the AI could choose whatever it want
+        elif action == 17:
+            response = self.client.set(
+                "food"
+            )  # TODO: he AI could choose whatever it want to drop
+        elif action == 18:
+            response = self.client.incantation()
         else:
             reward = -0.5
+
+        if self.client.is_dead or response == "dead":
+            terminated = True
+            reward = -100.0
+        elif response == "ok":
+            reward = 1.0
+        elif response == "ko":
+            reward = -0.1
 
         if not terminated:
             observation = self._get_real_observation()
@@ -81,18 +101,26 @@ class ZappyEnv(gym.Env):
 
     def _get_real_observation(self):
         obs = np.zeros(657, dtype=np.int32)
-        self.client.send("Inventory\n")
-        self.client.receive_line()
-        # TODO: string invetory to maths
 
-        self.client.send("Look\n")
-        self.client.receive()
+        inv = self.client.inventory()
+        res = inv.split(" ")
+        i = 0
+        for item in res:
+            obs[i] = int(item)
+            i += 1
+        if inv and type(inv) is not str:
+            pass
 
-        # TODO: vision logic
+        vision_list = self.client.look()
+        if vision_list and type(vision_list) is list:
+            pass
+
+        obs[655] = self.client.level
+        obs[656] = inv.food
 
         return obs
 
     def close(self):
         if self.client:
-            pass
+            self.client.close()
         self.server_manager.stop()
