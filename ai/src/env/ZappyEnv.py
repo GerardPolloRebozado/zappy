a3a4e3f1ce172bdf32eb7949d616005f155f1ea7
Pol Cortadellas Palomino <pol.cortadellas-palomino@epitech.eu>
2026-06-09 00:10:28 +0200

    chore: zappyenv rewards and observation logic

--- ai/src/env/ZappyEnv.py
+++ ai/src/env/ZappyEnv.py
@@ -2,14 +2,18 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 from server_manager import ServerManager
+from src.client.ai_client import ZappyAiClient
 
 
 class ZappyEnv(gym.Env):
-    def __init__(self, port=4242, ip="127.0.0.1"):
+    def __init__(self, port=4242, ip="127.0.0.1", team_name="TeamAI"):
         super(ZappyEnv, self).__init__()
         self.ip = ip
+        self.port = port
+        self.team_name = team_name
         self.server_manager = ServerManager(port=port)
         self.client = None
+
         self.action_space = spaces.Discrete(19)
         self.action_mapping = {
             1: "Forward",
@@ -25,35 +29,70 @@ class ZappyEnv(gym.Env):
             17: "Set",
             18: "Incantation",
         }
-        # shape = 81 vision tiles * 8 posible things in a tile + 7 inventory slots + lvl + food left
+
+        # shape = 81 vision tiles * 8 posibles recursos + 7 inventario + nivel + vida
         self.observation_space = spaces.Box(
             low=0, high=10000, shape=(657,), dtype=np.int32
         )
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
+
         self.server_manager.start()
-
-        # TODO: make handshake, fill observation values
-
-        observation = np.zeros(657, dtype=np.int32)
+        self.client = ZappyAiClient(self.port, self.team_name, self.ip)
+        self.client.connect()
+        self.client.receive_line()
+        self.client.receive_line()
+        self.client.receive_line()
+        observation = self._get_real_observation()
         info = {}
+
         return observation, info
 
     def step(self, action):
         command = self.action_mapping.get(action, None)
 
-        if command is not None:
-            # TODO: send command and wait answer
-            pass
-
-        observation = np.zeros(657, dtype=np.int32)
         reward = 0.0
-        terminated = False  # TODO: kill bot if its dead
+        terminated = False
         truncated = False
-        info = {}
 
+        if command is not None:
+            self.client.send(f"{command}\n")
+
+            response = self.client.receive().strip()
+
+            if response == "dead":
+                terminated = True
+                reward = -100.0
+            elif response == "ok":
+                reward = 1.0
+            elif response == "ko":
+                reward = -0.1
+        else:
+            reward = -0.5
+
+        if not terminated:
+            observation = self._get_real_observation()
+        else:
+            observation = np.zeros(657, dtype=np.int32)
+
+        info = {}
         return observation, reward, terminated, truncated, info
 
+    def _get_real_observation(self):
+        obs = np.zeros(657, dtype=np.int32)
+        self.client.send("Inventory\n")
+        self.client.receive_line()
+        # TODO: string invetory to maths
+
+        self.client.send("Look\n")
+        self.client.receive()
+
+        # TODO: vision logic
+
+        return obs
+
     def close(self):
+        if self.client:
+            pass
         self.server_manager.stop()
