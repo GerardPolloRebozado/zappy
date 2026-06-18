93c5e43d1881f93a78ebee81b16245d2390ccb80
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-18 11:48:57 +0200

    fix(commands): separate network from action

--- gui/src/Systems/ParticleSystem.cpp
+++ gui/src/Systems/ParticleSystem.cpp
@@ -1,7 +1,9 @@
 #include "Systems/ParticleSystem.hpp"
 #include "Components/ComponentInhabitant.hpp"
+#include "Components/ComponentMusic.hpp"
 #include "Components/ComponentParticleEmitter.hpp"
 #include "Components/ComponentShared.hpp"
+#include "Components/ComponentTags.hpp"
 #include <cstdlib>
 
 namespace zappy {
@@ -11,6 +13,39 @@ static float randf(float min, float max) {
 }
 
 void ParticleSystem::update(World& w, float dt) {
+    auto eventStorage = w.get_storage<EventEggHatched>();
+    if (eventStorage) {
+        std::vector<Entity> eventsToRemove;
+        for (auto const& [entity, event] : *eventStorage) {
+            auto posComponent = w.get_component<Position>(entity);
+            if (posComponent) {
+                ComponentParticleEmitter emitter;
+                emitter.isPlaying = true;
+                emitter.loop = false;
+                emitter.duration = 0.1f;
+                emitter.emitRate = 500.0f;
+                emitter.minLifetime = 0.5f;
+                emitter.maxLifetime = 1.5f;
+                emitter.minSize = 0.05f;
+                emitter.maxSize = 0.15f;
+                emitter.minVelocity = raylib::Vector3(-3.0f, 4.0f, -3.0f);
+                emitter.maxVelocity = raylib::Vector3(3.0f, 8.0f, 3.0f);
+                emitter.colorPalette = {raylib::Color::Red(),     raylib::Color::Green(),
+                                        raylib::Color::Blue(),    raylib::Color::Yellow(),
+                                        raylib::Color::Magenta(), raylib::Color::SkyBlue(),
+                                        raylib::Color::Orange()};
+
+                w.add_component(entity, emitter);
+                w.add_component(entity, std::make_shared<ComponentMusic>(
+                                            std::string("assets/sounds/effect/egg_layed.mp3")));
+            }
+            eventsToRemove.push_back(entity);
+        }
+        for (const auto& e : eventsToRemove) {
+            w.remove_component<EventEggHatched>(e);
+        }
+    }
+
     auto storage = w.get_storage<ComponentParticleEmitter>();
     if (!storage) {
         return;
