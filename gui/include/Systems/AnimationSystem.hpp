f72de2e6b3ef96b5ac5100862b6a09c90562e3cd
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-20 20:31:44 +0200

    chore: move render systems to folder

--- gui/include/Systems/AnimationSystem.hpp
+++ gui/include/Systems/AnimationSystem.hpp
@@ -6,24 +6,17 @@
 #include "Graphics/AssetManager.hpp"
 
 namespace zappy {
-/**
- * @class AnimationSystem
- * @brief System responsible for processing entity animations and movement interpolation.
- *
- * This system iterates over entities with Animation and MovementInterpolation components
- * to advance their visual positions and animation frames smoothly based on the server time
- * frequency.
- */
 class AnimationSystem {
   public:
     AnimationSystem() = default;
     ~AnimationSystem() = default;
 
-    /**
-     * @brief Update the movement interpolation and animation frames for all relevant entities.
-     * @param w The game world containing the entities.
-     */
     void update(World& w);
+
+  private:
+    void _update2DMovement(World& w, float freq);
+    void _update3DMovement(World& w, float freq);
+    void _updateAnimations(World& w);
 };
 } // namespace zappy
 
--- gui/include/Systems/ParticleSystem.hpp
+++ gui/include/Systems/ParticleSystem.hpp
@@ -8,6 +8,13 @@ namespace zappy {
 class ParticleSystem {
   public:
     void update(World& w, float dt);
+
+  private:
+    void _handleEggs(World& w);
+    void _handleDeaths(World& w);
+    void _handleIncantationStart(World& w);
+    void _handleIncantationEnd(World& w);
+    void _updateEmitters(World& w, float dt);
 };
 
 } // namespace zappy
