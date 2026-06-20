140d947494c05e24365370e72a89d2b9665af020
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-20 20:38:12 +0200

    fix: add docs back

--- gui/include/Systems/AnimationSystem.hpp
+++ gui/include/Systems/AnimationSystem.hpp
@@ -6,11 +6,23 @@
 #include "Graphics/AssetManager.hpp"
 
 namespace zappy {
+/**
+ * @class AnimationSystem
+ * @brief System responsible for processing entity animations and movement interpolation.
+ *
+ * This system iterates over entities with Animation and MovementInterpolation components
+ * to advance their visual positions and animation frames smoothly based on the server time
+ * frequency.
+ */
 class AnimationSystem {
   public:
     AnimationSystem() = default;
     ~AnimationSystem() = default;
 
+    /**
+     * @brief Update the movement interpolation and animation frames for all relevant entities.
+     * @param w The game world containing the entities.
+     */
     void update(World& w);
 
   private:
