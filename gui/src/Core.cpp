6e382f38057727200410d3bc2eddc2ad3080e94c
Pol Cortadellas Palomino <pol.cortadellas-palomino@epitech.eu>
2026-06-21 18:44:54 +0200

    chore: music on core constructor

--- gui/src/Core.cpp
+++ gui/src/Core.cpp
@@ -48,11 +48,6 @@ Core::Core(int port, const std::string& host) : _port(port), _host(host) {
     AssetManager::getInstance().loadAll();
     raylib::Window::HideCursor();
 
-    // Add the background music
-    auto backgroundMusic = _world.spawn();
-    _world.add_component(backgroundMusic,
-                         std::make_shared<ComponentMusic>(
-                             AssetManager::getInstance().getMusicPath("country"), true));
     _chatLogs = std::make_shared<ChatLogs>();
     FactoryCommands::setChatLogs(_chatLogs);
     _uiManager = std::make_shared<UIManager>();
