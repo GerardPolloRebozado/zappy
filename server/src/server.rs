9dfb58bd4ba84172a5d5ef0785feb5afdb10aec4
Gerard Du Pre <gerardit4@gmail.com>
2026-06-22 14:33:23 +0200

    perf: :zap: improve server performance by sending more at the same time

--- server/src/server.rs
+++ server/src/server.rs
@@ -190,7 +190,8 @@ impl Server {
         for entity in requests_to_write {
             let mut responses = Vec::new();
             if let Some(c) = self.world.get_component_mut::<NetworkData>(entity) {
-                responses = c.pending_responses.clone();
+                let limit = std::cmp::min(c.pending_responses.len(), 300);
+                responses = c.pending_responses.drain(..limit).collect();
             }
             for response in responses {
                 self.handle_response(entity, response);
