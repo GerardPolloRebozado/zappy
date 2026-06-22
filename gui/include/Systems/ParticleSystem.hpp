8f9ad529e98c1c7d913b09e2c5cbec59cdc0c9cc
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-22 09:40:46 +0200

    feat(particle): broadcast particle effect

--- gui/include/Systems/ParticleSystem.hpp
+++ gui/include/Systems/ParticleSystem.hpp
@@ -14,6 +14,7 @@ class ParticleSystem {
     void _handleDeaths(World& w);
     void _handleIncantationStart(World& w);
     void _handleIncantationEnd(World& w);
+    void _handleBroadcast(World& w);
     void _updateEmitters(World& w, float dt);
 };
 
--- gui/src/Systems/Particles/ParticleSystem.cpp
+++ gui/src/Systems/Particles/ParticleSystem.cpp
@@ -15,6 +15,7 @@ void ParticleSystem::update(World& w, float dt) {
     _handleDeaths(w);
     _handleIncantationStart(w);
     _handleIncantationEnd(w);
+    _handleBroadcast(w);
     _updateEmitters(w, dt);
 }
 
