27d7efdb2187eead9df4fade7a718591ecd1dc94
Victor Ripert <victor.ripert@epitech.eu>
2026-06-23 15:12:19 +0200

    fix conflict again

--- gui/src/CoreUI.cpp
+++ gui/src/CoreUI.cpp
@@ -272,18 +272,6 @@ void Core::_setupGameUI() {
     auto sun = _world.spawn();
     _world.add_component(sun, std::make_shared<CelestialObject>(3.0f, 0.0015f, "sun"));
 
-
-    // Spawn Background parallax
-    auto backgroundParallaxEntity = _world.spawn();
-    _world.add_component(backgroundParallaxEntity, std::make_shared<BackgroundParallax>());
-
-    // Spawn Moon & Sun
-    auto moon = _world.spawn();
-    _world.add_component(moon, std::make_shared<CelestialObject>(0.0f, 0.01f, "moon"));
-    auto sun = _world.spawn();
-    _world.add_component(sun, std::make_shared<CelestialObject>(3.0f, 0.0015f, "sun"));
-
-
     // Chat Panel
     float chatY = (float)_window->GetHeight() - 240.0f;
     _uiManager->addComponent(std::make_shared<UIChatPanel>(
