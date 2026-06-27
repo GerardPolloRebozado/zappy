f277ddff24418a9d810be284fc24a271a42c21e2
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2026-06-27 10:44:07 +0200

    refactor: command updates use tagging and simulation system

--- gui/include/Commands/AResourceCommand.hpp
+++ gui/include/Commands/AResourceCommand.hpp
@@ -38,41 +38,6 @@ class AResourceCommand : public ACommand {
         return {pId, static_cast<ResourceType>(rId), true};
     }
 
-    void updateInventory(Inventory* inv, ResourceType resourceId, int delta) {
-        if (!inv) {
-            return;
-        }
-        switch (resourceId) {
-            case ResourceType::FOOD:
-                inv->food += delta;
-                inv->exactHp += delta * 126.0f;
-                if (inv->exactHp > inv->maxHp) {
-                    inv->maxHp = inv->exactHp;
-                }
-                break;
-            case ResourceType::LINEMATE:
-                inv->linemate += delta;
-                break;
-            case ResourceType::DERAUMERE:
-                inv->deraumere += delta;
-                break;
-            case ResourceType::SIBUR:
-                inv->sibur += delta;
-                break;
-            case ResourceType::MENDIANE:
-                inv->mendiane += delta;
-                break;
-            case ResourceType::PHIRAS:
-                inv->phiras += delta;
-                break;
-            case ResourceType::THYSTAME:
-                inv->thystame += delta;
-                break;
-            default:
-                break;
-        }
-    }
-
     struct PlayerContext {
         Entity entity;
         Position pos;
@@ -92,32 +57,6 @@ class AResourceCommand : public ACommand {
             }
         }
         return {Entity(0, 0), {0, 0}, false};
-    }
-
-    void updateTileInventory(Position playerPos, ResourceType resourceId, int delta, World& world) {
-        auto tileTagStorage = world.get_storage<TileTag>();
-        if (!tileTagStorage) {
-            return;
-        }
-
-        for (auto const& [ent, tag] : *tileTagStorage) {
-            auto tPos = world.get_component<Position>(ent);
-            if (tPos && tPos->x == playerPos.x && tPos->y == playerPos.y) {
-                Entity food = world.spawn();
-                world.add_component<AnimatedResource>(food, {resourceId, false});
-                world.add_component<Position>(food, tPos);
-                world.add_component<Animation>(food, {"", 0.0f, 60.0f, 1.0f, true});
-                world.add_component<MovementInterpolation3D>(food, {static_cast<float>(playerPos.x),
-                                                                    static_cast<float>(playerPos.y),
-                                                                    0.0f, false, 2.0f});
-
-                auto tileInv = world.get_component<Inventory>(ent);
-                if (tileInv) {
-                    updateInventory(tileInv.get(), resourceId, delta);
-                }
-                break;
-            }
-        }
     }
 };
 } // namespace zappy
